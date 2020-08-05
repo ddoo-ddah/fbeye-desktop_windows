b43da5d759d2684096056efec3c35e7de72af26d
susemeeee <hffg123@naver.com>
2020-08-05 22:16:30 +0900

    Add Main class

---
+++ src/com/FBEye/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Main.java
+ * Author : susemeeee
+ * Created Date : 2020-08-05
+ */
+package com.FBEye;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
