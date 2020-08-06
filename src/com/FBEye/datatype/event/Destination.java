d19b8f7fea20d55602a78bf2da3815a379955828
susemeeee <hffg123@naver.com>
2020-08-06 16:21:13 +0900

    Add Destination

---
+++ src/com/FBEye/datatype/event/Destination.java
@@ -0,0 +1,13 @@
+/*
+ * Destination.java
+ * Author : susemeeee
+ * Created Date : 2020-08-06
+ */
+package com.FBEye.datatype.event;
+
+public enum Destination {
+    NONE,
+    SERVER,
+    MANAGER,
+    LOGIN_PAGE
+}
