cef4905c21a66bb925840331892b6c5b38931462
susemeeee <hffg123@naver.com>
2020-08-06 17:12:58 +0900

    Fix calculate error in ViewDisposer

--- src/com/FBEye/util/ViewDisposer.java
+++ src/com/FBEye/util/ViewDisposer.java
@@ -10,11 +10,11 @@ import java.awt.*;
 public class ViewDisposer {
     public static Point getLocation(int x, int y){
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        return new Point(x / 1500 * screenSize.width, y / 1000 * screenSize.width);
+        return new Point((int)(((double)x / 1500) * screenSize.width), (int)((double)y / 1000 * screenSize.height));
     }
 
     public static Dimension getSize(int width, int height){
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        return new Dimension(width / 1500 * screenSize.width, height / 1000 * screenSize.height);
+        return new Dimension((int)(((double)width / 1500) * screenSize.width), (int)((double)height / 1000 * screenSize.height));
     }
 }
