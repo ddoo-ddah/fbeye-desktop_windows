f56fa090a09015d284f0ed78dc4aff576b4a2cdd
susemeeee <hffg123@naver.com>
2020-08-18 16:59:25 +0900

    Add SignalDataMaker

---
+++ src/com/FBEye/util/SignalDataMaker.java
@@ -0,0 +1,12 @@
+/*
+ * SignalDataMaker.java
+ * Author : susemeeee
+ * Created Date : 2020-08-18
+ */
+package com.FBEye.util;
+
+public class SignalDataMaker {
+    public static String make(String signal){
+        return "\"data\":\"" + signal + "\"";
+    }
+}
