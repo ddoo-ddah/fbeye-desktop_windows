43afc42c5d5e7d92ac55c94320961168efc08c06
susemeeee <hffg123@naver.com>
2020-08-20 17:21:24 +0900

    Add BlankFrame

---
+++ src/com/FBEye/UI/BlankFrame.java
@@ -0,0 +1,37 @@
+/*
+ * BlankFrame.java
+ * Author : susemeeee
+ * Created Date : 2020-08-20
+ */
+package com.FBEye.UI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class BlankFrame {
+    private JFrame frame;
+
+    public BlankFrame(){
+        init();
+    }
+
+    private void init(){
+        frame = new JFrame();
+        frame.setLayout(null);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation(new Point(0,0));
+        frame.setSize(screenSize);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+        frame.setAlwaysOnTop(true);
+        frame.setUndecorated(true);
+        frame.setVisible(true);
+    }
+
+    public JFrame getFrame(){
+        return frame;
+    }
+}
