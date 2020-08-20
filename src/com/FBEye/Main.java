09b2e7791359eaaab7bb15194ca257d9ee159fd1
susemeeee <hffg123@naver.com>
2020-08-20 17:22:36 +0900

    Update to blind other screens

--- src/com/FBEye/Main.java
+++ src/com/FBEye/Main.java
@@ -5,11 +5,22 @@
  */
 package com.FBEye;
 
+import com.FBEye.UI.BlankFrame;
 import com.FBEye.UI.FBEyeFrame;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         System.setProperty("file.encoding", "UTF-8");
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice[] devices = ge.getScreenDevices();
+        for(int i = 0; i < devices.length; i++){
+            devices[i].setFullScreenWindow(new BlankFrame().getFrame());
+            ((JFrame)devices[i].getFullScreenWindow()).getContentPane().setBackground(Color.BLACK);
+            devices[i].getFullScreenWindow().repaint();
+        }
         new FBEyeFrame();
     }
 }
